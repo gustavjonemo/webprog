6c4bb794eefd3b37cbddec6058a5464613e16ff2
gustavjonemo <gustav-jonemo@hotmail.com>
2022-01-28 10:36:47 +0100

    Update lab1.js
    Salad class implemented

--- labs/lab1/lab1.js
+++ labs/lab1/lab1.js
@@ -7,6 +7,7 @@
 const imported = require("./inventory.js");
 //console.log(imported.inventory['Sallad']);
 /*
+
 console.log('Object.keys():')
 let names = Object.keys(imported.inventory);
 names
@@ -21,16 +22,15 @@ names
  */
 
 console.log('\n--- Assignment 1 ---------------------------------------');
+
 function getKeyByValue(object, value) {
     return Object.keys(object).find(key => object[key] === value);
-  }
+}
 
 function makeOptions(inventory, type){
     let prprts = Object.values(inventory);
     return prprts.reduce((ingredients, newIngr) => {
         if (!!newIngr[type]) {
-            //console.log(getKeyByValue(inventory, newIngr));
-            //console.log(ingredients);
             let name = getKeyByValue(inventory, newIngr)
             return ingredients + '<option value="' + name + '"> ' + name + ', ' + newIngr['price'] + ' kr</option>\n'
         } else {
@@ -39,12 +39,43 @@ function makeOptions(inventory, type){
     }, '');
 }
 
-console.log(makeOptions(imported.inventory, 'foundation'));
-//makeOptions(imported.inventory, 'foundation');
-
+//console.log(makeOptions(imported.inventory, 'foundation'));
 
 console.log('\n--- Assignment 2 ---------------------------------------')
 /*
+class Salad {
+    ingredients;
+
+    constructor(){
+        this.ingredients = new Map();
+    };
+
+    add(name, properties){
+        this.ingredients.set(name, properties);
+        return this;
+    }; // return this object to make it chainable
+
+    remove(name){
+        this.ingredients.delete(name);
+        return this;
+    }; // return this object to make it chainable
+}
+*/
+
+class Salad {
+    //constructor(){};
+
+    add(name, properties){
+        this[name] = properties;
+        return this;
+    };
+
+    remove(name){
+        delete this[name];
+        return this;
+    };
+}
+
 let myCaesarSalad = new Salad()
 .add('Sallad', imported.inventory['Sallad'])
 .add('Kycklingfilé', imported.inventory['Kycklingfilé'])
@@ -56,7 +87,7 @@ let myCaesarSalad = new Salad()
 console.log(JSON.stringify(myCaesarSalad) + '\n');
 myCaesarSalad.remove('Gurka');
 console.log(JSON.stringify(myCaesarSalad) + '\n');
-*/
+
 console.log('\n--- Assignment 3 ---------------------------------------')
 //console.log('En ceasarsallad kostar ' + myCaesarSalad.getPrice() + 'kr');
 // En ceasarsallad kostar 45kr
